e2660afe73dd264f881b1a74fd1f472c48f6c878
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-10-27 14:13:53 +0200

    finished basic configurations

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import router from './notifications/notification.routes';
-const { protector } = require('pickly-protector-master');
+// const { protector } = require('pickly-protector-master');
 
 // const userEnricher = async (user) => {
 // 	let mongoUser = await User.findOne({ email: user.tokeninfo.email });
@@ -11,10 +11,10 @@ const { protector } = require('pickly-protector-master');
 // 		});
 // 	return mongoUser;
 // };
-var serviceAccount = require('./service-account.json');
+// var serviceAccount = require('./service-account.json');
 
 const app: express.Application = express();
-app.use(protector(serviceAccount));
+// app.use(protector(serviceAccount));
 app.use('/notifications', router);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './app';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: __dirname + '../secrets/.env' });
+dotenv.config({ path: __dirname + '/../secrets/.env' });
 
 mongoose
 	.connect(process.env.DB_URI!, {
@@ -12,6 +12,6 @@ mongoose
 	})
 	.then(() => console.log('connected to db'));
 
-app.listen(3001, () => {
-	console.log('Listening on port 3001');
+app.listen(process.env.PORT, () => {
+	console.log(`Listening on port ${process.env.PORT}`);
 });
